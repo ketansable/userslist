38b671b17a007663824196e6bc0985e9055a8614
ketansable <ketansable45@gmail.com>
2019-06-19 21:08:59 +0530

    Sorting implemented

--- GitUserList/src/app/app.component.ts
+++ GitUserList/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
+
 import { GitUsers } from "./models/git-users";
 import { GitUsersService } from "./services/git-users.service";
 
@@ -13,7 +14,7 @@ import { GitUsersService } from "./services/git-users.service";
 export class AppComponent {
   	title = 'GitUserList';
 	gitUsers: GitUsers[];
-	gitUserDetails: GitUsers[];
+	gitUserDetails: any = {};
 	search: Subject<string> = new Subject<string>();
 	cache = {
         gitUsers: []
@@ -33,6 +34,7 @@ export class AppComponent {
 		this.gitUsersService.getUsers().subscribe(res => {
 			this.cache.gitUsers = res;
 			this.gitUsers = res;
+			this.sortUsersList('login');
 		}, error => {
             console.log(error);
         });
@@ -40,7 +42,7 @@ export class AppComponent {
 
 	getUserDetails(login) { 
 		this.gitUsersService.getUsers(login).subscribe(res => {
-			this.gitUserDetails = res;
+			this.gitUserDetails[login] = res;
 		}, error => {
             console.log(error);
         });
@@ -53,5 +55,18 @@ export class AppComponent {
             //if empty search box we restore first users
             this.gitUsers = this.cache.gitUsers;
         }
+	}
+
+	sortUsers(val: any) {
+		this.sortUsersList(val);
+	}
+
+	sortUsersList(value: any) {
+		if(value === 'login') {
+			this.gitUsers.sort((prev,next) => prev.login.localeCompare(next.login));
+		}		
+		else if(value === 'rank') {
+			this.gitUsers.sort((prev,next) => { return parseInt(prev.id) - parseInt(next.id)});
+		}			
 	}
 }
